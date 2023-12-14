748791e5ec9d577fd392403ca8e69a49d8378b7f
myroslavaKukhta <miroslavakukhta@gmail.com>
2023-12-14 14:33:32 +0200

    Add item form

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
+
+type AddItemFormPropsType = {
+    addItem: (title: string, todolistId: string) => void
+    id: string
+}
+
+const AddItemForm = (props: AddItemFormPropsType) => {
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== '') {
+            props.addItem(title.trim(), props.id);
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.key === 'Enter') {
+            addItem()
+        }
+    }
+    return (
+        <div>
+            <input value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   className={error ? 'error' : ''}
+            />
+
+
+            <button onClick={addItem}>+</button>
+            {error && <div className="error-message">{error}</div>}
+        </div>
+    )
+}
+
+export default AddItemForm;
